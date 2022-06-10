fc32a646711caeb637c137d5aa1b8f7b3f6807ff
Алена Кондратьева <554103869@mail.ru>
2022-06-10 15:49:31 +0000

    Delete and rename

--- src/CloudStorage.FileManagerService/Controllers/FileManagerController.cs
+++ src/CloudStorage.FileManagerService/Controllers/FileManagerController.cs
@@ -123,7 +123,7 @@ namespace CloudStorage.FileManagerService.Controllers
                 Tree = tree
             });
         }
-            
+
         /// <summary>
         /// запрос на содание файла
         /// </summary>
@@ -140,7 +140,7 @@ namespace CloudStorage.FileManagerService.Controllers
             {
                 return UnprocessableEntity();
             }
-            
+
             FileInfo fileInfo = new FileInfo(Path.Combine(contentPath, fileRequest.Name));
             if (fileInfo.Exists)
             {
@@ -161,9 +161,9 @@ namespace CloudStorage.FileManagerService.Controllers
 
         [HttpPost]
         [ActionName("upload")]
-        public IActionResult StorageUpload([FromQuery(Name = "disk")] string disk, 
+        public IActionResult StorageUpload([FromQuery(Name = "disk")] string disk,
                                            [FromQuery(Name = "path")] string path,
-                                           [FromQuery(Name = "overwrite")] int overwrite, 
+                                           [FromQuery(Name = "overwrite")] int overwrite,
                                            [FromQuery(Name = "files[]")] List<IFormFile> files)
         {
             string diskPath = GetDiskPath(disk);
@@ -185,6 +185,79 @@ namespace CloudStorage.FileManagerService.Controllers
             {
                 Result = new(Status.Success, "uploaded"),
             });
+        }
+
+        /// <summary>
+        /// переименовть объект
+        /// </summary>
+        /// <param name="request">тело запроса</param>
+        /// <returns>результат запроса</returns>
+        [HttpPost]
+        [ActionName("rename")]
+        public IActionResult Rename([FromBody] RenameRequest request)
+        {
+            string diskPath = GetDiskPath(request.Disk);
+            //string contentPath = GetContentPath(diskPath, fileRequest.Path);
+
+            if (request == null || string.IsNullOrWhiteSpace(request.OldName) || string.IsNullOrWhiteSpace(request.NewName))
+            {
+                return UnprocessableEntity();
+            }
+
+            if (request.Type == EntityType.File)
+            {
+                if (!System.IO.File.Exists(Path.Combine(diskPath, request.OldName)))
+                    return Ok(new Result(Status.Warning, "fileNotExist"));
+
+                System.IO.File.Move(Path.Combine(diskPath, request.OldName), Path.Combine(diskPath, request.NewName));
+            }
+            else
+            {
+                if (!Directory.Exists(Path.Combine(diskPath, request.OldName)))
+                    return Ok(new Result(Status.Warning, "dirNotExist"));
+
+                Directory.Move(Path.Combine(diskPath, request.OldName), Path.Combine(diskPath, request.NewName));
+            }
+
+            return Ok(new Result(Status.Success, "renamed"));
+        }
+
+        /// <summary>
+        /// переименовть объект
+        /// </summary>
+        /// <param name="request">тело запроса</param>
+        /// <returns>результат запроса</returns>
+        [HttpPost]
+        [ActionName("delete")]
+        public IActionResult Delete([FromBody] DeleteRequest request)
+        {
+            string diskPath = GetDiskPath(request.Disk);
+            //string contentPath = GetContentPath(diskPath, fileRequest.Path);
+
+            if (request == null || request.Items == null)
+            {
+                return UnprocessableEntity();
+            }
+
+            foreach (Item Item in request.Items)
+            {
+                if (Item.Type == EntityType.File)
+                {
+                    if (!System.IO.File.Exists(Path.Combine(diskPath, Item.Path)))
+                        continue;
+
+                    System.IO.File.Delete(Path.Combine(diskPath, Item.Path));
+                }
+                else
+                {
+                    if (!Directory.Exists(Path.Combine(diskPath, Item.Path)))
+                        continue;
+
+                    Directory.Delete(Path.Combine(diskPath, Item.Path));
+                }
+            }
+
+            return Ok(new Result(Status.Success, "deleted"));
         }
 
     }
