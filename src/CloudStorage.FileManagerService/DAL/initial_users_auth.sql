742d0568d75d6203a6e29c1e62a205b2f99eb1df
BogdanovFA <acoustic925@gmail.com>
2022-07-21 10:03:38 +0300

    Добавил скрипт для создания бд + таблица для юзеров. Добавил ConnectionString.

---
+++ src/CloudStorage.FileManagerService/DAL/initial_users_auth.sql
@@ -0,0 +1,15 @@
+﻿create database users_auth_db;
+
+create table users_auth
+(
+	id uuid not null,
+	user_name varchar(100) not null,
+	user_password varchar(100) not null,
+	reg_date timestamp not null
+);
+
+create unique index users_auth_id_uix on users_auth(id);
+
+create unique index users_auth_pk on users_auth(id);
+
+create unique index users_auth_name_password_uix on users_auth(user_name, user_password);
