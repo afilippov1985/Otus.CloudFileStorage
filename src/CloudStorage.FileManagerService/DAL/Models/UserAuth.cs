bd1255d0cc6c4857324f883fcf3a148c73abdc6a
BogdanovFA <acoustic925@gmail.com>
2022-07-21 10:12:24 +0300

    Добавил модель пользователя

---
+++ src/CloudStorage.FileManagerService/DAL/Models/UserAuth.cs
@@ -0,0 +1,31 @@
+﻿#nullable disable
+
+namespace CloudStorage.FileManagerService.DAL.Models
+{
+
+    /// <summary>
+    /// Модель для аутентификации пользователя
+    /// </summary>
+    public class UserAuth
+    {
+        /// <summary>
+        /// Идентификатор
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// Имя пользователя
+        /// </summary>
+        public string UserName { get; set; }
+        
+        /// <summary>
+        /// Пароль
+        /// </summary>
+        public string UserPassword { get; set; }
+
+        /// <summary>
+        /// Дата регистрации
+        /// </summary>
+        public DateTime RegDate { get; set; }
+    }
+}
