3423929daae443c7bb1d9f7bb33bdec1548a85f9
Andrey Filippov <afilippov2009@mail.ru>
2022-08-22 21:59:51 +0400

    Регистрация

--- src/CloudStorage.FileManagerService/Controllers/AuthController.cs
+++ src/CloudStorage.FileManagerService/Controllers/AuthController.cs
@@ -84,6 +84,64 @@ namespace CloudStorage.FileManagerService.Controllers
         }
 
         [HttpPost]
+        [ActionName("reg")]
+        public IActionResult Register([FromBody] LoginRequest request)
+        {
+            DAL.Models.UserAuth? user = null;
+            using (var db = new ApplicationDbContext())
+            {
+                user = db.Users.Add(new DAL.Models.UserAuth() {
+                    UserName = request.Login,
+                    UserPassword = new DAL.Models.PasswordHasher().HashPassword(null, request.Password),
+                    RegDate = DateTime.Now,
+                });
+                db.SaveChanges();
+            }
+
+            var claims = new List<Claim> {
+                new Claim("Id", user.Id.ToString()),
+                new Claim(ClaimTypes.Name, user.UserName),
+            };
+
+            var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+            var authProperties = new AuthenticationProperties()
+            {
+                //     //AllowRefresh = <bool>,
+                //     // Refreshing the authentication session should be allowed.
+
+                ExpiresUtc = DateTimeOffset.UtcNow.AddMinutes(20),
+                //     // The time at which the authentication ticket expires. A 
+                //     // value set here overrides the ExpireTimeSpan option of 
+                //     // CookieAuthenticationOptions set with AddCookie.
+
+                //     //IsPersistent = true,
+                //     // Whether the authentication session is persisted across 
+                //     // multiple requests. When used with cookies, controls
+                //     // whether the cookie's lifetime is absolute (matching the
+                //     // lifetime of the authentication ticket) or session-based.
+
+                //     //IssuedUtc = <DateTimeOffset>,
+                //     // The time at which the authentication ticket was issued.
+
+                //     //RedirectUri = <string>
+                //     // The full path or absolute URI to be used as an http 
+                //     // redirect response value.
+            };
+
+            HttpContext.SignInAsync(
+                CookieAuthenticationDefaults.AuthenticationScheme,
+                new ClaimsPrincipal(claimsIdentity), authProperties);
+
+            Console.WriteLine($"User {user.UserName} logged in at {user.RegDate}.");
+
+            return Ok(new LoginResponse()
+            {
+                User = user,
+            });
+        }
+
+        [HttpPost]
         [ActionName("logout")]
         public IActionResult Logout()
         {
