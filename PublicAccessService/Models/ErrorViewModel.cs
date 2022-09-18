2745190b89c1532583610ed39ebc6185fbcee6d3
Andrey Filippov <afilippov2009@mail.ru>
2022-09-18 17:23:31 +0400

    Добавлен проект

---
+++ PublicAccessService/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PublicAccessService.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
